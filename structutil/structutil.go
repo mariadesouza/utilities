292a6764d0a2758f4e978ed07352b9f7934318ac
Maria De Souza <maria.g.desouza@gmail.com>
2017-09-09 17:52:38 -0700

    Update structutil.go

--- structutil/structutil.go
+++ structutil/structutil.go
@@ -1,18 +1,21 @@
+// Package structutil provides functions to easily work woth structs
+
 package structutil
 
 import (
 	"reflect"
 )
 
+// StructToMap : returns a map of names to values for struct passed .
 func StructToMap(v interface{}) *map[string]interface{} {
 
 	//modelRefType := v.Type()
 	structmap := make(map[string]interface{})
 
-	s := reflect.ValueOf(v).Elem() //element values
-	typeofT := s.Type()            // type of Element - name of element
-	for i := 0; i < s.NumField(); i++ {
-		f := s.Field(i)
+	elements := reflect.ValueOf(v).Elem() //element values
+	typeofT := elements.Type()            // type of Element - name of element
+	for i := 0; i < elements.NumField(); i++ {
+		f := elements.Field(i)
 		elementName := typeofT.Field(i).Name
 		//typeOfStructElement := f.Type()
 		valueOfElement := f.Interface()
